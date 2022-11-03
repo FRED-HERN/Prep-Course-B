483dc357db564b6cd4d6d20168b16f4e91f58402
Fredy Hernández <dspublicitarias@gmail.com>
2022-11-03 10:33:47 -0500

    Solución 05

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,18 +8,38 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var suma = 0
+    for (var i = 0; i <= 10; i++) {
+      suma = suma + i
+    }
+return suma 
+     
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+var pares = 0
+
+for (var i = 0 ; i < array.length % 2 === 0; i ++ ) {
+
+  pares = array[i].length
+}
+return pares
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+
+//  var cuadrado = 0
+
+//for (var i = 0 ; array[i].length <= array ; i++) {
+//  cuadrado = array[i].length**2
+//}
+//return cuadrado
 }
 
 function sumaArray(array){
